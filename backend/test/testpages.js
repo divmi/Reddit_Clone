b9a1bdd9d4ee557e62bb77b997acb4a3d9771e17
Divya Mittal <divyacs1991@gmail.com>
2021-05-14 21:08:07 -0700

    Get test case

--- backend/test/testpages.js
+++ backend/test/testpages.js
@@ -1,16 +1,16 @@
-var app = require("../server.js");
+const app = require("../index.js");
 var chai = require("chai");
 chai.use(require("chai-http"));
 var expect = require("chai").expect;
 
 var agent = require("chai").request.agent(app);
 const auth =
-  "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwODFiYmJhNWJiMDNjNjJlYzAwYzg4MiIsImlhdCI6MTYxOTMyMDI4NywiZXhwIjoxNjIwMzI4Mjg3fQ.2XDTN56AhUl7uYNCS5lQG8dcZZJHtX7R0oY1hN7phgo";
+  "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTEsImlhdCI6MTYyMTA0OTM3OCwiZXhwIjoxNjIyMDU3Mzc4fQ.GBTpfXBydRXHEvil84sJgzNRTM9306ozj0OMLR0ZfCw";
 
 it("POST /loginUser", function (done) {
   agent
-    .post("/login/loginUser")
-    .send({ username: "divyamittal@gmail.com", password: "abcd" })
+    .post("/login")
+    .send({ email: "divya@gmail.com", password: "abcd" })
     .set("Authorization", auth)
     .then(function (res) {
       expect(res).to.have.status(200);
@@ -21,11 +21,11 @@ it("POST /loginUser", function (done) {
     });
 });
 
-describe("Splitwise App", function () {
-  it("GET transaction/getTransactionInfo - get info", function (done) {
+describe("Reddit App", function () {
+  it("GET ", function (done) {
     agent
-      .get("transaction/getTransactionInfo?ID=6081bfb742f00d4d8ec7d1b8")
-      .set("Authorization", auth)
+      .get("/getUserProfileByMongoID?ID=609f4022e8ab974852c6020a")
+      .set("authorization", auth) //
       .then(function (res) {
         expect(res).to.have.status(200);
         done();
@@ -35,10 +35,10 @@ describe("Splitwise App", function () {
       });
   });
 
-  it("Get /getTransactionFromUser - Detail", function (done) {
+  it("Get /getCommunitiesForOwner - get Community", function (done) {
     agent
       .get(
-        "transaction/getTransactionFromUser?ID=6081bbba5bb03c62ec00c882&page=0&size=2"
+        "/getCommunitiesForOwner?ID=609f4022e8ab974852c6020a&page=0&size=2&search=''"
       )
       .set("Authorization", auth)
       .then(function (res) {
@@ -50,9 +50,9 @@ describe("Splitwise App", function () {
       });
   });
 
-  it("GET /getCommentForTransaction - get comment", function (done) {
+  it("GET /getMessageUserNames", function (done) {
     agent
-      .get("/comment/getCommentForTransaction?transID=6082643ffaab8711dae2c757")
+      .get("/getMessageUserNames?ID=51")
       .set("Authorization", auth)
       .then(function (res) {
         expect(res).to.have.status(200);
@@ -63,9 +63,9 @@ describe("Splitwise App", function () {
       });
   });
 
-  it("Get /getGroupSummary - Group Member Names", function (done) {
+  it("Get /getCommunities - Get Communities", function (done) {
     agent
-      .get("/group/getGroupSummary?ID=6081bbba5bb03c62ec00c882")
+      .get("/getCommunities")
       .set("Authorization", auth)
       .then(function (res) {
         expect(res).to.have.status(200);
